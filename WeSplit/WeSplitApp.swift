d4349366936424c068e816ea20b4e709e4aa0768
Midhet Sulemani <midhetfatema94@gmail.com>
2020-11-04 15:07:05 +0300

    First commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Waveline Media on 11/4/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
